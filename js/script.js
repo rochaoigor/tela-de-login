4bde6adf11fb15a6d37d5e68dff01deac1243ad9
rochaoigor <igorsantooos@gmail.com>
2022-11-23 12:59:37 -0300

    Atualização de needs-validation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-let form = document.querySelector('form')
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-})
+(function(){
+    let forms = document.querySelectorAll('.needs-validation');
+
+    Array.prototype.slice.call(forms)
+    .forEach(function(form){
+        form.addEventListener('submit', function(e){
+        if(!form.checkValidity()) {
+            e.preventDefault();
+            e.stopPropagation();
+        }
+        form.classList.add('was-validated');
+        })
+    }, false);
+})();
